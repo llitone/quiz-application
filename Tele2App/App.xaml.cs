906711e7644b2f5af809324f16af4f6ba60d2154
Сергей Чуприн <sergeychuprin2006@gmail.com>
2023-04-18 14:12:07 +0300

    create app

---
+++ Tele2App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Tele2App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Tele2App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Tele2App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
