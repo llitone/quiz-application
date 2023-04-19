6587bc6f0cfca146116bf313e19ad4475a447685
Сергей Чуприн <sergeychuprin2006@gmail.com>
2023-04-19 17:26:41 +0300

    questions and subjects models

---
+++ Tele2App/Model/Question.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Tele2App.Model
+{
+    public class Question
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("age")]
+        public int Age { get; set; }
+        [JsonPropertyName("question")]
+        public string Text { get; set; }
+        [JsonPropertyName("difficulty")]
+        public int Difficulty { get; set; }
+        [JsonPropertyName("value")]
+        public int Points { get; set; }
+        [JsonPropertyName("subject_id")]
+        public int SubjectId { get; set; }
+        [JsonPropertyName("explanation")]
+        public string Explanation { get; set; }
+    }
+}
---
+++ Tele2App/Model/Subject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Tele2App.Model
+{
+    public class Subject
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("subject")]
+        public string Name { get; set; }
+        public IEnumerable<Question> Questions { get; set; }
+    }
+}
