38ca713dc6befc043cb49eeb3d5ef01297264809
D1ffic00lt <dm.filinov@gmail.com>
2023-04-19 23:07:32 +0300

    add subjects list

--- server/test.py
+++ server/test.py
@@ -2,4 +2,4 @@ from pprint import pprint
 
 import requests
 
-pprint(requests.get("http://127.0.0.1:5000/app/api/v1.0/questions/").text)
+pprint(requests.get("http://127.0.0.1:5000/app/api/v1.0/subjects/").text)
