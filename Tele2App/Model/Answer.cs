8ab112a239ce916522105c0094eeac13875d8edb
Сергей Чуприн <sergeychuprin2006@gmail.com>
2023-04-20 10:45:20 +0300

    add answer and question models

---
+++ Tele2App/Model/Answer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Tele2App.Model
+{
+    public class Answer
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("question_id")]
+        public int QuestionId { get; set; }
+        [JsonPropertyName("answer")]
+        public string Text { get; set; }
+        [JsonPropertyName("is_correct")]
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ Tele2App/Model/QuestionsData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Tele2App.Model
+{
+    public class QuestionsData
+    {
+        [JsonPropertyName("question")]
+        public List<Question> Questions { get; set; }
+    }
+}
