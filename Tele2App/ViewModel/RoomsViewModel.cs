40898eb7f70add0b5b855bc817c6c3c88976bef7
Sillerus <moskovkin2006@yandex.ru>
2023-04-21 01:22:15 +0300

    Добавлено: кнопка создания комнаты(работающая), страница добавления комнаты с полями ввода и выбора.

---
+++ Tele2App/ViewModel/RoomsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Tele2App.View;
+
+namespace Tele2App.ViewModel;
+
+public class RoomsViewModel : BaseViewModel
+{
+    private string _nameroom;
+
+    public string NameRoom
+    {
+        get { return (_nameroom); }
+        set
+        {
+            _nameroom = value;
+            Notify(nameof(NameRoom));
+        }
+    }
+
+    public ButtonCommand CreateRoom
+    {
+        get
+        {
+            return new ButtonCommand(async () =>
+            {
+                await Shell.Current.GoToAsync(nameof(RoomsPage));
+            });
+        }
+    }
+    public ButtonCommand AddRoom
+        {
+            get
+            {
+                return new ButtonCommand(async () =>
+                {
+                    await Shell.Current.GoToAsync(nameof(AddRoomPage));
+                });
+            }
+        }
+}
